8c7848b45fa66240d14bc681d02a227ede3d5289
atronah <atronah.ds@gmail.com>
2017-10-19 22:41:54 +0300

    docs(1xbasic/lst): adds comments and renames variables

--- src/1-basic/lst.erl
+++ src/1-basic/lst.erl
@@ -7,62 +7,78 @@
         , filter/2
         , reverse/1
         , concat/1
+        , flatten/1
+        , rdna/1
         ]).
 
-create_test() -> [1, 2, 3, 4, 5] = create(5).
+%% @doc makes list with numbers from `1` to `N` (inckuding).
 create(N) -> create_p(N, []).
-create_p(0, L) -> L;
-create_p(N, L) -> create_p(N-1, [N|L]).
-        
-create_backward_test() -> [4, 3, 2, 1] = create_backward(4).
-create_backward(N) -> create_backward_p(N, []).
-create_backward_p(0, L) -> L;
-create_backward_p(N, L) -> create_backward_p(N-1, L ++ [N]).
+create_p(0, List) -> List;
+create_p(N, List) -> create_p(N-1, [N|List]).
+create_test() -> [1, 2, 3, 4, 5] = create(5).
 
+%% @doc makes list with numbers from `N` to `1` (including).
+create_backward(N) -> create_backward_p(N, []).
+create_backward_p(0, Result) -> Result;
+create_backward_p(N, Result) -> create_backward_p(N-1, Result ++ [N]).
+create_backward_test() -> [4, 3, 2, 1] = create_backward(4).
+
+%% @doc prints numbers from `1` to `N` (including), each with new line.
 print_range(1) -> io:format("~p~n", [1]);
 print_range(N) ->
     print_range(N-1),
     io:format("~p~n", [N]).
 
+%% @doc prints odd numbers from `1` to `N` (including), each with new line.
 print_range_odd(1) -> io:format("~p~n", [1]);
 print_range_odd(N) when N rem 2 =/= 0 -> 
     print_range_odd(N-1),
     io:format("~p~n", [N]);
 print_range_odd(N) -> print_range_odd(N-1).
 
+%% @doc makes list containing numbers from `List` which less than `Max` or equal it
+filter(List, Max) -> [Item || Item <- List, Item =< Max].
 filter_test() -> [1, 2, 3] = filter([1, 2, 3, 4, 5], 3).
-filter(L, M) -> [X || X <- L, X =< M].
 
+%% @doc makes list with reversed items of `List`
+reverse(List) -> reverse_p(List, []).
+reverse_p([], Result) -> Result;
+reverse_p([Head|Tail], Result) -> reverse_p(Tail, [Head|Result]).
 reverse_test() -> [3, 2, 1] = reverse([1, 2, 3]).
-reverse(L) -> reverse_p(L, []).
-reverse_p([], R) -> R;
-reverse_p([H|T], R) -> reverse_p(T, [H|R]).
 
+%% @doc concatinates items from nested list of `List`
+concat(List) -> reverse(concat_p(List, [])).
+concat_p([[SubHead|SubTail]|Tail], Result) -> concat_p([SubTail|Tail], [SubHead|Result]); 
+concat_p([[]|Tail], Result) -> concat_p(Tail, Result);
+concat_p([Head|Tail], Result) -> concat_p(Tail, [Head|Result]);
+concat_p([], Result) -> Result.
 concat_test() -> [1, 2, 3, 4, 5, 6, ok, 7, 8, 9] = concat([[1, 2], [], [3, 4, 5, 6], ok, [7, 8, 9]]).
-concat(L) -> reverse(concat_p(L, [])).
-concat_p([[HS|TS]|T], R) -> concat_p([TS|T], [HS|R]); 
-concat_p([[]|T], R) -> concat_p(T, R);
-concat_p([H|T], R) -> concat_p(T, [H|R]);
-concat_p([], R) -> R.
 
+%% @doc transforms `List` with nested lists to flat view 
+flatten(List) -> flatten_p(List, []).
+flatten_p([[Head|Tail]], Result) -> flatten_p([Head|Tail], Result);
+flatten_p([[]|Tail], Result) -> flatten_p(Tail, Result);
+flatten_p([Head|Tail], Result) -> flatten_p(concat(Tail), [Head|Result]);
+flatten_p([], Result) -> reverse(Result).
 flatten_test() -> [1, 2, 3, 4, 5, 6] = flatten([[1, [2, [3], []], [[[4]]], [5, 6]]]).
-flatten(L) -> flatten_p(L, []).
-flatten_p([[H|T]], R) -> flatten_p([H|T], R);
-flatten_p([[]|T], R) -> flatten_p(T, R);
-flatten_p([H|T], R) -> flatten_p(concat(T), [H|R]);
-flatten_p([], R) -> reverse(R).
 
-rdna_test() -> [u, u, a, c, c, g, a, a] = rdna("AATGGCTT").
+%% @doc transforms DNA to RDNA
+%% @param DNA sequence of nucleatides, represented either as a list of atoms (`[a, Tail, g, c]`) or as a uppercase string (`ATGC`)
+%% @returns sequence of nucleatides complimentary to `DNA` nucleatides and represented as a list of atoms (`[a, u, g, c]`)
 rdna(DNA) -> rdna_p(DNA, []).
-rdna_p([H|T], R) -> rdna_p(T, [nucl_compliment(nucl_atom(H))|R]);
-rdna_p([], R) -> reverse(R).
+rdna_p([Head|Tail], Result) -> rdna_p(Tail, [nucl_compliment(nucl_atom(Head))|Result]);
+rdna_p([], Result) -> reverse(Result).
+rdna_test() -> [u, u, a, c, c, g, a, a] = rdna("AATGGCTT").
+
+%% @doc tranform letter representaion of nucleatide to atom representation
 nucl_atom($G) -> g; 
 nucl_atom($C) -> c; 
 nucl_atom($T) -> t;
 nucl_atom($A) -> a;
-nucl_atom(N) -> N.
+nucl_atom(Other) -> Other.
+
+%% @doc transform DNA nucleatide to complimentary RDNA nucleatide
 nucl_compliment(c) -> g;
 nucl_compliment(g) -> c;
 nucl_compliment(t) -> a;
 nucl_compliment(a) -> u.
-
