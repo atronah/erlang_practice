ff9e6c9a29560388b71c9b7ac4d314e837176d16
atronah <atronah.ds@gmail.com>
2017-10-24 00:36:54 +0300

    ft(1-basic/json): adds value validation functions

--- src/1-basic/json.erl
+++ src/1-basic/json.erl
@@ -28,9 +28,38 @@ read_error_test() -> {error, not_found} = read(z, new([{x, [1, 2, 3]}, {y, [4, 5
 %% @doc puts new `Value` into `JSON` with `Key`
 write(Key, Value, JSON) -> 
 	case JSON of
-		#{Key := OldValue} -> JSON#{Key := Value};
+		#{Key := _OldValue} -> JSON#{Key := Value};
 		_Other ->  {error, not_found}
 	end.
 write_ok_test() -> #{x := true, y := false} = write(y, false, new([{x, true}, {y, true}])). 
 write_error_test() -> {error, not_found} = write(z, false, new([{x, true}, {y, true}])). 
 
+%% @doc checks that `Value` is string (list of integers)
+is_string([Head|Tail]) when is_integer(Head) -> 
+	is_string_p(Tail, true).
+is_string_p([], true) -> true;
+is_string_p(_Other, _Status) -> false.
+
+%% @doc checks that `Value` matches the specification `Key = string()`
+is_Key(Value) -> is_string(Value).
+
+%% @doc checks that `Value` matches the specification `KeySpec = string()`
+is_KeySpec(Value) -> is_string(Value).
+
+%% @doc checks that `Value` matches the specification `BasicValue = string() | boolean() | integer() | float()`
+is_BasicValue(Value) -> is_string(Value) 
+                        orelse is_boolean(Value)
+                        orelse is_integer(Value)
+                        orelse is_float(Value).
+
+%% @doc checks that passed argument matches the specification `{Key, ValueSpec}` 
+is_Key_ValueSpec_Pair({Key, ValueSpec}) -> is_Key(Key) andalso is_ValueSpec(ValueSpec);
+is_Key_ValueSpec_Pair(_Other) -> false.
+
+%% @doc checks that `Value` matches the specification `ValueSpec = BasicValue | [BasicValue] | {Key, ValueSpec} | [{Key, ValueSpec}]`
+is_ValueSpec([Head|Tail]) -> is_BasicValue(Head) andalso is_ValueSpec_list(Tail, is_BasicValue)
+                            orelse is_Key_ValueSpec_Pair(Head) andalso is_ValueSpec_list(Tail, is_Key_ValueSpec_Pair);
+is_ValueSpec(Value) -> is_BasicValue(Value) orelse is_Key_ValueSpec_Pair(Value).
+is_ValueSpec_list([Head|Tail], Checker) -> Checker(Head) andalso is_ValueSpec_list(Tail, Checker);
+is_ValueSpec_list([], _Checker) -> true;
+is_ValueSpec_list(_Other, _Checker) -> false.
