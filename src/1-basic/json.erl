787b25b96e329f720a224c2cb41cc10e070f4311
atronah <atronah.ds@gmail.com>
2017-10-23 19:27:54 +0300

    ft(1-basic/json): adds implementation for read function

--- src/1-basic/json.erl
+++ src/1-basic/json.erl
@@ -16,7 +16,15 @@ new_not_list_input_test() -> {error, bad_input} = new(1).
 new_not_tuple_input_test() -> {error, bad_input} = new([1]).
 
 %% @doc returns value from JSON with key `Key`.
-read(_Key, _JSON) -> not_implemented.
+read(Key, JSON) -> read_p(Key, JSON).
+read_p(Key, JSON) -> 
+	case JSON of
+		#{Key := Value} -> {ok, Value};
+		_ ->  {error, not_found}
+	end.
+read_ok_test() -> {ok, [4, 5, 6]} = read(y, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
+read_error_test() -> {error, not_found} = read(z, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
+
 
 %% @doc puts new `Value` into `JSON` with `Key`
 write(_Key, _Value, _JSON) -> not_implemented.
