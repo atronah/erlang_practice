ed143adf01356c2170161b9cd87118f4c5b1d2ce
atronah <atronah.ds@gmail.com>
2017-10-23 23:50:09 +0300

    ft(1-basic/json): adds implementation of write function.

--- src/1-basic/json.erl
+++ src/1-basic/json.erl
@@ -16,17 +16,21 @@ new_not_list_input_test() -> {error, bad_input} = new(1).
 new_not_tuple_input_test() -> {error, bad_input} = new([1]).
 
 %% @doc returns value from JSON with key `Key`.
-read(Key, JSON) -> read_p(Key, JSON).
-read_p(Key, JSON) -> 
+read(Key, JSON) ->
 	case JSON of
 		#{Key := Value} -> {ok, Value};
-		_ ->  {error, not_found}
+		_Other ->  {error, not_found}
 	end.
 read_ok_test() -> {ok, [4, 5, 6]} = read(y, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
 read_error_test() -> {error, not_found} = read(z, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
 
 
 %% @doc puts new `Value` into `JSON` with `Key`
-write(_Key, _Value, _JSON) -> not_implemented.
-
+write(Key, Value, JSON) -> 
+	case JSON of
+		#{Key := OldValue} -> JSON#{Key := Value};
+		_Other ->  {error, not_found}
+	end.
+write_ok_test() -> #{x := true, y := false} = write(y, false, new([{x, true}, {y, true}])). 
+write_error_test() -> {error, not_found} = write(z, false, new([{x, true}, {y, true}])). 
 
