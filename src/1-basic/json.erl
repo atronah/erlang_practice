16e4935ba8d59acedef235bb85e079e1285b727b
atronah <atronah.ds@gmail.com>
2017-10-24 19:11:00 +0300

    ft(1-basic/json): adds input validation for exported functions

--- src/1-basic/json.erl
+++ src/1-basic/json.erl
@@ -6,7 +6,10 @@
 
 
 %% @doc creates new JSON object from list `List` of pairs {`Key`, `Value`}
-new(List) -> new_p(List, maps:new()).
+new(List) -> 
+	if is_ValueSpec_list(List, is_Key_ValueSpec_Pair) -> new_p(List, maps:new());
+		true -> {error, bad_input}
+	end.
 new_p([], Map) -> Map;
 new_p([{Key, Value}|Tail], Map) -> new_p(Tail, maps:put(Key, Value, Map));
 new_p(_, _) -> {error, bad_input}.
@@ -17,9 +20,12 @@ new_not_tuple_input_test() -> {error, bad_input} = new([1]).
 
 %% @doc returns value from JSON with key `Key`.
 read(Key, JSON) ->
-	case JSON of
-		#{Key := Value} -> {ok, Value};
-		_Other ->  {error, not_found}
+	if is_KeySpec(Key) ->
+		case JSON of
+			#{Key := Value} -> {ok, Value};
+			_Other ->  {error, not_found}
+		end;
+		true -> {error, bad_input}
 	end.
 read_ok_test() -> {ok, [4, 5, 6]} = read(y, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
 read_error_test() -> {error, not_found} = read(z, new([{x, [1, 2, 3]}, {y, [4, 5, 6]}])).
@@ -27,10 +33,12 @@ read_error_test() -> {error, not_found} = read(z, new([{x, [1, 2, 3]}, {y, [4, 5
 
 %% @doc puts new `Value` into `JSON` with `Key`
 write(Key, Value, JSON) -> 
-	case JSON of
-		#{Key := _OldValue} -> JSON#{Key := Value};
-		_Other ->  {error, not_found}
-	end.
+	if is_KeySpec(Key) andalso is_ValueSpec(Value) ->
+		case JSON of
+			#{Key := _OldValue} -> JSON#{Key := Value};
+			_Other ->  {error, not_found}
+		end;
+		true -> {error, bad_input}.
 write_ok_test() -> #{x := true, y := false} = write(y, false, new([{x, true}, {y, true}])). 
 write_error_test() -> {error, not_found} = write(z, false, new([{x, true}, {y, true}])). 
 
