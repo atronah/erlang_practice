6a84a1ef315662d27e78d54db4a1ee917e9a3b40
atronah <atronah.ds@gmail.com>
2017-11-06 21:25:32 +0300

    rf(1-basic/calc): adds comments, tests and indents

--- src/1-basic/calc.erl
+++ src/1-basic/calc.erl
@@ -15,34 +15,42 @@
 %% - `unkwn` for others
 %% and `Lexeme` is value of `Token` in `String`
 tokenize(String) -> lists:reverse(tokenize_p(String, {}, [])).
-tokenize_p([], {TokenType, Lexeme}, Result) -> 
-	[{TokenType, lists:reverse(Lexeme)}|Result];
-tokenize_p([], _Other, Result) -> Result;
-tokenize_p([Head|Tail], Token, Result) 
-  when Head >= $0 andalso Head =< $9 -> 
+% return result with last Token (with reversed Lexeme) in the head.
+tokenize_p([], {TokenType, Lexeme}, Result) ->
+    [{TokenType, lists:reverse(Lexeme)} | Result];
+% process digit from the input string
+tokenize_p([Head | Tail], Token, Result)
+  when Head >= $0 andalso Head =< $9 ->
     case Token of
-        {num, Lexeme} -> tokenize_p(Tail, {num, [Head|Lexeme]}, Result);
-        {TokenType, Lexeme} -> tokenize_p(Tail, 
-					  {num, [Head]}, 
-					  [{TokenType, lists:reverse(Lexeme)}|Result]);
+        % add digit to the continuous sequence of digits being processed
+        {num, Lexeme} -> tokenize_p(Tail, {num, [Head | Lexeme]}, Result);
+        % move previous Token from buffer at the beggining of result
+        % and start processing new (numeric) token (put it into buffer)
+        {TokenType, Lexeme} -> tokenize_p(Tail
+                                            , {num, [Head]}
+                                            , [{TokenType, lists:reverse(Lexeme)} | Result]);
+        % start processing numeric Token
         _ -> tokenize_p(Tail, {num, [Head]}, Result)
     end;
-tokenize_p([Head|Tail], Token, Result) ->
+% process non digit char from input string
+tokenize_p([Head | Tail], Token, Result) ->
     Type = case Head of
                 $( -> lp;
                 $) -> rp;
                 $~ -> sign;
                 $- -> minus;
                 $+ -> plus;
-		$* -> multi;
-		$  -> wp;
+        $* -> multi;
+        $  -> wp;
                 _ -> unkwn
           end,
     case Token of
-        {TokenType, Lexeme} -> tokenize_p(Tail, 
-					  {Type, [Head]}, 
-					  [{TokenType, lists:reverse(Lexeme)}|Result]);
-        _ -> tokenize_p(Tail, {}, [{Type, [Head]}|Result])
+        % move previous token from buffer at the beggining of result and put current token into buffer
+        {TokenType, Lexeme} -> tokenize_p(Tail
+                                            , {Type, [Head]}
+                                            , [{TokenType, lists:reverse(Lexeme)} | Result]);
+        % put new token into buffer
+        _ -> tokenize_p(Tail, {}, [{Type, [Head]} | Result])
     end.
 tokenize_test() -> [{lp, "("}, {lp, "("},
 		    {num, "2"}, {plus, "+"}, {num, "3"},
