df5617df2011109942c3f8ee3def3c32b0a79ca9
atronah <atronah.ds@gmail.com>
2017-11-15 21:25:57 +0300

    ft(2-advanced/db): adds comments

--- src/2-advanced/db.erl
+++ src/2-advanced/db.erl
@@ -9,12 +9,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
+%% @doc create new empty `Db`
 new() -> {}.
 
 
+%% @doc release resources used for `Db`
 destroy(_Db) -> ok.
 
 
+%% @doc Adds/rewrites item of `Db` asociated with passed `Key`
 write(Key, Data, _Db = {}) ->
     {Key, Data, {}, {}};
 write(Key, Data, {RootKey, RootData, Smaller, Larger}) when Key < RootKey ->
@@ -23,9 +26,9 @@ write(Key, Data, {RootKey, RootData, Smaller, Larger}) when Key > RootKey ->
     {RootKey, RootData, Smaller, write(Key, Data, Larger)};
 write(Key, Data, {Key, _RootData, Smaller, Larger}) ->
     {Key, Data, Smaller, Larger}.
-    
 
 
+%% @doc Read `Data` from item of `Db` associated with passed `Key`
 read(Key, {RootKey, _RootData, Smaller, _Larger}) when Key < RootKey ->
     read(Key, Smaller);
 read(Key, {RootKey, _RootData, _Smaller, Larger}) when Key > RootKey ->
@@ -35,21 +38,23 @@ read(Key, {Key, Data, _Smaller, _Larger}) ->
 read(_Key, _Db) -> {error, not_found}.
 
 
+%% @doc Deletes item of `Db` with passed `Key`
 delete(Key, {RootKey, _RootData, Smaller, Larger}) when Key < RootKey ->
     {RootKey, _RootData, delete(Key, Smaller), Larger};
 delete(Key, {RootKey, _RootData, Smaller, Larger}) when Key > RootKey ->
     {RootKey, _RootData, Smaller, delete(Key, Larger)};
-delete(Key, {Key, _Data, {}, {}}) -> 
+delete(Key, {Key, _Data, {}, {}}) ->
     {};
-delete(Key, {Key, _Data, {SmallerKey, SmallerData, Smaller, Larger}, {}}) -> 
+delete(Key, {Key, _Data, {SmallerKey, SmallerData, Smaller, Larger}, {}}) ->
     {SmallerKey, SmallerData, Smaller, Larger};
-delete(Key, {Key, _Data, {}, {LargerKey, LargerData, Smaller, Larger}}) -> 
+delete(Key, {Key, _Data, {}, {LargerKey, LargerData, Smaller, Larger}}) ->
     {LargerKey, LargerData, Smaller, Larger};
-delete(Key, {Key, Data, Smaller, Larger}) -> 
+delete(Key, {Key, Data, Smaller, Larger}) ->
     delete(Key, rotate_right(Key, {Key, Data, Smaller, Larger}));
 delete(_Key, Db) -> Db.
 
 
+%% @doc Returns list of keys, which associated with passed `Data`
 match(Data, Db) -> match_p(Data, [Db], []).
 match_p(_Data, [], Result) ->
     Result;
@@ -59,28 +64,43 @@ match_p(Data, [{Key, Data, Smaller, Larger} | Tail], Result) ->
     match_p(Data, [Smaller, Larger | Tail], [Key | Result]);
 match_p(Data, [{_Key, _OtherData, Smaller, Larger} | Tail], Result) ->
     match_p(Data, [Smaller, Larger | Tail], Result).
-    
 
 
-
+% accessory function to rotate tree (needs to delete nodes with both of children)
 rotate_right_test_() ->
     [
         ?_assertEqual({error, not_found}, rotate_right(key, {})),
-        ?_assertEqual({6, "6"
-                        , {2, "2"
-                            , {1, "1", {}, {}}
-                            , {4, "4"
-                                , {3, "3", {}, {}}
-                                , {5, "5", {}, {}}}}
-                        , {}}
-                        , rotate_right(4
-                                        , {6, "6"
-                                            , {4, "4"
-                                                , {2, "2"
-                                                    , {1, "1", {}, {}}
-                                                    , {3, "3", {}, {}}}
-                                                , {5, "5", {}, {}}}
-                                            , {}}))
+        {"Check ratating right around element '4' for following tree
+                6
+               /
+              4
+             / \
+            2   5
+           / \
+          1   3
+         to get following tree:
+              6
+             /
+            2
+           / \
+          1   4
+             / \
+            3   5 "
+            , ?_assertEqual({6, "6"
+                            , {2, "2"
+                                , {1, "1", {}, {}}
+                                , {4, "4"
+                                    , {3, "3", {}, {}}
+                                    , {5, "5", {}, {}}}}
+                            , {}}
+                            , rotate_right(4
+                                            , {6, "6"
+                                                , {4, "4"
+                                                    , {2, "2"
+                                                        , {1, "1", {}, {}}
+                                                        , {3, "3", {}, {}}}
+                                                    , {5, "5", {}, {}}}
+                                                , {}}))}
     ].
 rotate_right(Key, {RootKey, _RootData, Smaller, Larger}) when Key < RootKey ->
     {RootKey, _RootData, rotate_right(Key, Smaller), Larger};
@@ -89,4 +109,4 @@ rotate_right(Key, {RootKey, _RootData, Smaller, Larger}) when Key > RootKey ->
 rotate_right(Key, {Key, Data, {SKey, SData, SSmaller, SLarger}, Larger}) ->
     {SKey, SData, SSmaller, {Key, Data, SLarger, Larger}};
 rotate_right(_Key, _Db) -> {error, not_found}.
-    
+
